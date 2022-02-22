9777aa5ac4d57a0698eb289a1fbf29a6f18ac07e
Obi Bruno <46797841+obibruno@users.noreply.github.com>
2022-02-22 14:20:44 -0600

    Search Bar Component has been added.

--- RedRockTrails/src/screens/TrailsScreen.js
+++ RedRockTrails/src/screens/TrailsScreen.js
@@ -1,10 +1,11 @@
 import react from "react";
 import { StyleSheet, View, Text } from "react-native";
-
+import SearchBar from "../reusables/SearchBar";
 const TrailsScreen = () => {
   return (
     <View>
       <Text>Trails Screen</Text>
+      <SearchBar />
     </View>
   );
 };
