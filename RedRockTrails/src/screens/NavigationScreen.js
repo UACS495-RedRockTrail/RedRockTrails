b1c0a42b8d75e22bdd6a94ab0ca81dbe2f21481f
Harris Crews <whc1998@yahoo.com>
2022-03-02 09:16:12 -0600

    Update NavigationScreen.js

--- RedRockTrails/src/screens/NavigationScreen.js
+++ RedRockTrails/src/screens/NavigationScreen.js
@@ -5,17 +5,6 @@ import { StyleSheet, View, Text } from "react-native";
 
 const NavigationScreen = () => {
   return (
-<<<<<<< HEAD
-    <WebView
-      source={{
-        uri: "https://www.google.com/maps/d/embed?mid=1FDyfj-hKq_DN-VN6w3YuV9lImjw&ehbc=2E312F",
-      }}
-    />
-  );
-};
-
- export default NavigationScreen;
-=======
     <View style={{ backgroundColor: "rgba(0,200,0,0.1)", flex: 1 }}>
       <View style={style.container}>
         <Text>Navigation Screen</Text>
@@ -35,4 +24,3 @@ const style = StyleSheet.create({
 });
 
 export default NavigationScreen;
->>>>>>> 05fca86edcedc4ece381fc063f5f64ecafd4c25f
