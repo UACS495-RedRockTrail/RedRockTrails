59d2161acc3fead58cba637f59f9392451db9caa
Harris Crews <whc1998@yahoo.com>
2022-03-02 09:07:29 -0600

    Added Red Rock Trails Google maps view to Navigation Screen.

--- RedRockTrails/src/screens/NavigationScreen.js
+++ RedRockTrails/src/screens/NavigationScreen.js
@@ -1,14 +1,16 @@
 import react from "react";
+import { WebView } from 'react-native-webview';
 import { StyleSheet, View, Text } from "react-native";
+
 
 const NavigationScreen = () => {
   return (
-    <View>
-      <Text>Navigation Screen</Text>
-    </View>
+    <WebView
+      source={{
+        uri: "https://www.google.com/maps/d/embed?mid=1FDyfj-hKq_DN-VN6w3YuV9lImjw&ehbc=2E312F",
+      }}
+    />
   );
 };
 
-const style = StyleSheet.create({});
-
-export default NavigationScreen;
+ export default NavigationScreen;
