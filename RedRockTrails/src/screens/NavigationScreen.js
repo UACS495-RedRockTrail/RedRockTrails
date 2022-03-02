beb14bd70c9a6c6330161b658ddbc392a0505b9c
Harris Crews <whc1998@yahoo.com>
2022-03-02 09:19:02 -0600

    Update NavigationScreen.js
    Red Rock Trail System google maps webview implemented into NavigationScreen.js.

--- RedRockTrails/src/screens/NavigationScreen.js
+++ RedRockTrails/src/screens/NavigationScreen.js
@@ -7,7 +7,11 @@ const NavigationScreen = () => {
   return (
     <View style={{ backgroundColor: "rgba(0,200,0,0.1)", flex: 1 }}>
       <View style={style.container}>
-        <Text>Navigation Screen</Text>
+      <WebView
+        source={{
+          uri: "https://www.google.com/maps/d/embed?mid=1FDyfj-hKq_DN-VN6w3YuV9lImjw&ehbc=2E312F",
+        }}
+      />
       </View>
     </View>
   );
@@ -16,7 +20,6 @@ const NavigationScreen = () => {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 10,
     position: "absolute",
     height: "84.5%",
     width: "100%",
