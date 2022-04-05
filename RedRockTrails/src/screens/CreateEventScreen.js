89e3f3a796979dfa189198ecb1297d35c26621f1
Obi Bruno <46797841+obibruno@users.noreply.github.com>
2022-04-05 17:34:23 -0500

    Continuation of the Create Event Screen.

--- RedRockTrails/src/screens/CreateEventScreen.js
+++ RedRockTrails/src/screens/CreateEventScreen.js
@@ -12,7 +12,7 @@ import DateTimePicker from "@react-native-community/datetimepicker";
 import SelectDropdown from "react-native-select-dropdown";
 import * as ImagePicker from "expo-image-picker";
 
-const CreateEventScreen = () => {
+const CreateEventScreen = (props) => {
   const locationV = [
     "Trail1",
     "Trail2",
@@ -27,20 +27,23 @@ const CreateEventScreen = () => {
   const [image, setImage] = useState(null);
   const [location, setLocation] = useState(null);
 
-  submit = () => {
+  const submit = () => {
     let collection = {};
     collection.title = title;
     collection.date = date;
+
     console.warn(collection);
 
+    //objects are not null do the following two    FIXME
     //perform axios post request
+    //iprops.navigation.goBack();
   };
-  setDateV = (event, dateV) => {
+  const setDateV = (event, dateV) => {
     let fDate =
       dateV.getMonth() + 1 + "/" + dateV.getDate() + "/" + dateV.getFullYear();
     setDate(fDate);
   };
-  setTimeV = (event, timeV) => {
+  const setTimeV = (event, timeV) => {
     let fTime = timeV.getHours() + ":" + timeV.getMinutes();
     setTime(fTime);
   };
@@ -53,6 +56,31 @@ const CreateEventScreen = () => {
       quality: 1,
     });
     //SET image uri here setImage(uri) FIXME
+
+    /*const data = new FormData();
+  
+  data.append("uploadFile", {
+    name: filename,
+    type: filetype,
+    uri:
+      Platform.OS === "android"
+        ? fileuri
+        : fileuri.replace("file://", "")
+  });
+ var url = uploadDoc
+
+  axios.post(url, data, {headers: {
+    "Content-Type": "multipart/form-data",
+    Accept: "application/json",
+    Authorization: authToken
+
+  }})
+  .then((res) => {
+   
+  })
+  .catch((err) => {
+   
+  })*/
     console.log(result);
 
     if (!result.cancelled) {
@@ -63,58 +91,95 @@ const CreateEventScreen = () => {
   return (
     <View style={{ backgroundColor: "rgba(0,200,0,0.1)", flex: 1 }}>
       <View style={style.container}>
-        <Text>Hello</Text>
-        <TextInput
-          placeholder="Event Title"
-          style={StyleSheet.input}
-          onChangeText={(text) => setTitle(text)}
-        />
-
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={new Date()}
-          mode={"date"}
-          is24Hour={true}
-          display="default"
-          onChange={setDateV}
-        />
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={new Date()}
-          mode={"time"}
-          is24Hour={true}
-          display="default"
-          onChange={setTimeV}
-        />
-        <SelectDropdown
-          data={locationV}
-          onSelect={(selectedItem, index) => {
-            console.log(selectedItem, index);
-            setLocation(selectedItem);
-          }}
-          buttonTextAfterSelection={(selectedItem, index) => {
-            // text represented after item is selected
-            // if data array is an array of objects then return selectedItem.property to render after item is selected
-            return selectedItem;
-          }}
-          rowTextForSelection={(item, index) => {
-            // text represented for each item in dropdown
-            // if data array is an array of objects then return item.property to represent item in dropdown
-            return item;
-          }}
-          defaultButtonText={"Select Location: "}
-        />
-        <Button title="Pick an image from camera roll" onPress={pickImage} />
-        {image == null && (
-          <Image
-            source={require("../../assets/camera.png")}
-            style={{ width: 200, height: 200 }}
+        <View style={{ flexDirection: "row" }}>
+          <Text style={style.titleText}>Event Title:</Text>
+          <TextInput
+            placeholder="Red Rock Marathon"
+            style={style.input}
+            onChangeText={(text) => setTitle(text)}
           />
-        )}
-        {image != null && (
-          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-        )}
-
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={style.titleText}>Event Date:</Text>
+          <DateTimePicker
+            style={style.dateTimeStyle}
+            testID="dateTimePicker"
+            value={new Date()}
+            mode={"date"}
+            is24Hour={true}
+            display="default"
+            onChange={setDateV}
+          />
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={style.titleText}>Event Time:</Text>
+          <DateTimePicker
+            style={style.dateTimeStyle}
+            testID="dateTimePicker"
+            value={new Date()}
+            mode={"time"}
+            is24Hour={true}
+            display="default"
+            onChange={setTimeV}
+          />
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={style.titleText}>Event Location:</Text>
+          <View style={{ marginLeft: 15 }}>
+            <SelectDropdown
+              buttonStyle={{
+                backgroundColor: "rgba(0,200,0,0.1)",
+                borderRadius: 12,
+              }}
+              dropdownStyle={{
+                backgroundColor: "lightgrey",
+                borderRadius: 12,
+              }}
+              data={locationV}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index);
+                setLocation(selectedItem);
+              }}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                // text represented after item is selected
+                // if data array is an array of objects then return selectedItem.property to render after item is selected
+                return selectedItem;
+              }}
+              rowTextForSelection={(item, index) => {
+                // text represented for each item in dropdown
+                // if data array is an array of objects then return item.property to represent item in dropdown
+                return item;
+              }}
+              defaultButtonText={"Trail "}
+            />
+          </View>
+        </View>
+        <View>
+          <Button
+            title="Select an image from camera roll"
+            onPress={pickImage}
+            color="green"
+          />
+          {image == null && (
+            <Image
+              source={require("../../assets/camera.png")}
+              style={{ width: 200, height: 200, alignSelf: "center" }}
+            />
+          )}
+          {image != null && (
+            <Image
+              source={{ uri: image }}
+              style={{
+                width: 200,
+                height: 200,
+                alignSelf: "center",
+                borderColor: "lightgreen",
+                borderRadius: 15,
+                borderWidth: 5,
+              }}
+            />
+          )}
+        </View>
         <TouchableOpacity onPress={() => submit()} style={style.button}>
           <Text>Submit</Text>
         </TouchableOpacity>
@@ -132,6 +197,17 @@ const style = StyleSheet.create({
     width: "100%",
     justifyContent: "space-around",
   },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "green",
+  },
+  input: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 10,
+    color: "#B9A394",
+  },
   button: {
     backgroundColor: "green",
     height: 40,
@@ -140,6 +216,11 @@ const style = StyleSheet.create({
     alignItems: "center",
     alignSelf: "center",
     borderRadius: 10,
+  },
+  dateTimeStyle: {
+    height: 50,
+    width: 150,
+    justifyContent: "space-evenly",
   },
 });
 
