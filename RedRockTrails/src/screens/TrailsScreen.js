28c4d03056cb4c994816a9cdd08f9477f82a53d3
Harris Crews <whc1998@yahoo.com>
2022-04-17 09:46:37 -0500

    Implemented FlatList into TrailsScreen.js

--- RedRockTrails/src/screens/TrailsScreen.js
+++ RedRockTrails/src/screens/TrailsScreen.js
@@ -14,29 +14,6 @@ let hughKaul = {
   uri: "https://images.unsplash.com/photo-1605196560547-b2f7281b7355?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dHJhaWx8ZW58MHx8MHx8&w=1000&q=80",
 };
 
-let kiwanis = {
-  name: "Kiwanis Vulcan Trail",
-  corridor: "Jones Valley",
-  length: "2.28 miles",
-  rating: "5 Stars",
-  uri: "https://imgs.6sqft.com/wp-content/uploads/2017/01/10140523/hiking-trail.jpg",
-};
-
-let watkins = {
-  name: "Watkins Brook",
-  corridor: "Shades Creek",
-  length: "0.49 miles",
-  rating: "5 Stars",
-  uri: "https://images.unsplash.com/photo-1605196560547-b2f7281b7355?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dHJhaWx8ZW58MHx8MHx8&w=1000&q=80",
-};
-
-let clairmont = {
-  name: "Clairmont Walking Trail",
-  corridor: "Jones Valley",
-  length: "0.08 miles",
-  rating: "5 Stars",
-  uri: "https://imgs.6sqft.com/wp-content/uploads/2017/01/10140523/hiking-trail.jpg",
-};
 
 const TrailsScreen = ({navigation}) => {
   
@@ -50,31 +27,27 @@ const TrailsScreen = ({navigation}) => {
   function getTrails() {
     trailAPI.get('Trails/ListTrails').then(async function(response){
       setTrails(response.data); // Store trail data in array
-      console.log(trails);      // Print data to console to insure it's there
+      
     }).catch(function(error){
       console.log(error)        // Print any errors to console
     })
   }
 
   if (!trails) return null;
-
-
+  
   return (
 
     <View style={{ backgroundColor: "rgba(0,200,0,0.1)", flex: 1 }}>
       <View style={style.container}>
 
         <SearchBar />
-        <ScrollView style={{ flex: 1 }} contentContainerStyle={{ flexGrow: 1 }}>
-          <View style={style.itemContainer}>
-            <TrailPreview info={hughKaul} nav={navigation}/>
-            <TrailPreview info={kiwanis} nav={navigation}/>
-            <TrailPreview info={watkins} nav={navigation}/>
-            <TrailPreview info={clairmont} nav={navigation}/>
-            <TrailPreview info={clairmont} nav={navigation}/>
-            <TrailPreview info={clairmont} nav={navigation}/>
-          </View>
-        </ScrollView>
+        <FlatList 
+          data={trails.Trails}
+          keyExtractor={ (item) => item.Id}
+          renderItem={( {item} ) => (
+            <TrailPreview info={item} nav={navigation} />
+          )}
+        />
 
       </View>
     </View>
