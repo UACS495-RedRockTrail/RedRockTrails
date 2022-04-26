6d1e2151d4c10c240e987f9be774cc617da78f6a
Obi Bruno <46797841+obibruno@users.noreply.github.com>
2022-04-26 12:58:57 -0500

    Fixed AdoptATrailFormScreen
    Fixed styling and sizing of the screen.

--- RedRockTrails/src/screens/AdoptATrailFormScreen.js
+++ RedRockTrails/src/screens/AdoptATrailFormScreen.js
@@ -1,12 +1,28 @@
-import React, { Component } from "react";
+import react from "react";
 import { WebView } from "react-native-webview";
+import { StyleSheet, View, Text } from "react-native";
 
-export default AdoptATrailFormScreen = () => {
+const AdoptATrailFormScreen = () => {
   return (
-    <WebView
-      source={{
-        uri: "https://www.jotform.com/FreshwaterLT/adopt-a-trail-interest-form",
-      }}
-    />
+    <View style={{ backgroundColor: "rgba(0,200,0,0.1)", flex: 1 }}>
+      <View style={style.container}>
+        <WebView
+          source={{
+            uri: "https://www.jotform.com/FreshwaterLT/adopt-a-trail-interest-form",
+          }}
+        />
+      </View>
+    </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "absolute",
+    height: "84.5%",
+    width: "100%",
+  },
+});
+
+export default AdoptATrailFormScreen;
